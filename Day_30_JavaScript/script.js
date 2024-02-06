4aa2c4dc66cebf1ae9c13ccc7ce2ab59bbeb4f63
NeetuBinoriya <neetu.binoriya@gmail.com>
2024-02-06 12:58:50 +0530

    BootStarp and JavaScrip Intro

---
+++ Day_30_JavaScript/script.js
@@ -0,0 +1,65 @@
+// console.log("Hello World");
+// console.log("Hello Thor")
+// MultiLine Comment
+// /*{
+// 2+5
+// }*/
+// Sinlge Line Comment
+// ../
+
+// let number1 = 1000;
+// let number2 = 2000;
+
+// let ram = 20
+
+// console.log(number1+number2)
+// console.log(ram)
+// let number = 30.2
+// console.log(number)
+// let char = 'neetu'
+// console.log(char)
+// let Name = "Superman"
+// console.log(Name)
+
+// let is18 = true
+// console.log(is18)
+// let $dollar = 30000
+// console.log("I have ", $dollar,"$")
+
+// let name1 = "Superman";
+// let name2 = "Spiderman";
+// console.log(
+//   '"' +
+//     name1 +
+//     '"' +
+//     'is belongs to from "DC" and ' +
+//     '"' +
+//     name2 +
+//     '"' +
+//     " Belongs to from Marvel"
+// );
+
+
+// let name1 = "Spiderman";
+// let name2 = "Superman";
+// name1 = "Aquaman"
+
+// console.log(`"${name1}" is belong from "DC" and "${name2}" is belongs from "Marvel"`)
+
+// const number = 20;
+// // number = 30
+// console.log(number)
+
+// let number1 = 20;
+// let number2 = 30;
+// // console.log(number1++, ++number1)
+// console.log(++number1, number1++)
+// console.log(number1)
+
+
+const number = "20";
+
+// console.log(number == 20) // double equal to (==) for Value Check
+ console.log(number === 20) // triple equal to (===) for value and type Check
+
+// console.log(typeof(number))
