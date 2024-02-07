1f2181133883aedb2cc142cdb844360387bdfc36
NeetuBinoriya <neetu.binoriya@gmail.com>
2024-02-07 12:55:26 +0530

    Prompt If-Else Ternary Functions Intro

---
+++ Day_31_JavaScript/script.js
@@ -0,0 +1,84 @@
+//***** IF-Else****
+
+// const age = 18
+
+// const age = prompt("Enter Your Age");
+
+// console.log("User Enter Age : ",age)
+
+// if(age >= 18){
+//     const city = prompt ("Enter Your City")
+//     if(city!= "Indore"){
+//         console.log("You are not eligible")
+//     } else{
+//         console.log("You can Drive")
+//     }
+// }
+// else{
+//     console.log("You can not drive")
+// }
+
+// const $dollar = 500;
+// // const $dollar = 400;
+// if($dollar >= 500){
+//     console.log("You can Buy IPHONE...")
+// }
+// else{
+//     console.log("You have to sell Your Kidney..")
+// }
+
+// Ternary Operator
+// (10>5) ? (console.log("True")) : (console.log("False"))
+// (10>50) ? (console.log("True")) : (console.log("False"))
+// const $dollar = 500;
+// ($dollar>=500)
+// ? console.log("You can Buy IPHONE...")
+// : console.log("You have to sell Your Kidney..")
+
+// ***Switch Case***
+// const day = parseInt(prompt("Enter the day"));
+
+// // console.log(typeof(day))
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday, Holiday");
+//     break;
+//   default:
+//     console.log("Not a Valid Day");
+// }
+
+
+// ****Function***
+
+// Normal Function Declaration
+function print(){
+    console.log("This is a Function")
+}
+// print();
+// Funtion With Return Value
+function pi(){
+    // return 3.14
+    return "Superman"
+}
+// const result = pi()
+// console.log(result)
+console.log(pi())
