9b52e81c907ad39a5098107e7d2ffbf8f328271b
NeetuBinoriya <neetu.binoriya@gmail.com>
2024-02-14 12:13:00 +0530

    Array Methods

---
+++ Day_35_JavaScript/script.js
@@ -0,0 +1,41 @@
+// ***Array Methods***
+// let arr = [10,20,30,40,50];
+
+// ***Array Splice Methods***
+// it will update the Original Array
+// arr.splice(1,3) // will remove from 1 to 3 indexing elements
+// console.log(arr)
+
+// ***Array Slice Methods***
+// Sliced Return a brand new array, we have to pass the start end index of elements, it will exclude the last index
+// let slicedArray = arr.slice(1,2)
+
+//console.log("SlideArray ", slicedArray)
+
+
+// ***Array Concat Methods***
+// let arr1 = ["laptop", "smartphones", "smarwatch","ipad"];
+// let arr2 = ["orange", "Apple", "Mango","cherry"];
+
+// // console.log(arr1.concat(arr2))
+// let result = arr1.concat(arr2);
+// console.log(result);
+
+// ***Array Search  Methods***
+// ***Array IndexOf  Methods***
+// let arr = ["apple", "Orange", "Banana","apple","Mango","apple","Banana", "apple"];
+// Will show output which first element it searchs like there are 3 apples but he will show us first apple index
+// console.log("Index of Searching element = " +arr.indexOf("Apple"))
+// ***Array lastIndexOf  Methods***
+// const searchingElement = "Apple"
+
+// console.log("Last Index of Searching element = " +arr.lastIndexOf(searchingElement.toLowerCase()))
+// Will show output which Last element it searchs like there are 3 apples but he will show us last apple index
+
+
+// ***Array includes Methods***
+
+// Will check the element is available or not will give output in boolean like true or false
+
+const arr = [1,2,3,4,5,6]
+console.log("Check ", arr.includes(7));
