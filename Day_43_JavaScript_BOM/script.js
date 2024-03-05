230c7a5b8a82940108e0da681c2f4c43c6cb8eb7
NeetuBinoriya <neetu.binoriya@gmail.com>
2024-03-05 13:12:47 +0530

    BOM Methods

---
+++ Day_43_JavaScript_BOM/script.js
@@ -0,0 +1,21 @@
+console.log("JavaScript Bom");
+
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+
+// const innerHeight = window.innerHeight;
+// const innerWidth = window.innerWidth;
+
+// document.querySelector(
+//   ".superman"
+// ).innerHTML = `<h1>Browser InnerHeight = ${innerHeight}</h1> <h1>Browser InnerWidth = ${innerWidth}</h1>`;
+
+
+// // console.log(window.location)
+// const href = window.location.href;
+// const host = window.location.hostname;
+
+// document.querySelector(".bom").innerHTML = `<h1>href = ${href} </h1> <h3>HostName = ${host}</h3>`;
+
+
+
